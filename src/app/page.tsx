dfade8b6f279e5630a3d2c87c3ad9448f17d0eed
farrel irawan <irawanssfarrel@gmail.com>
2025-09-11 10:03:52 +0700

    adding separator component to the section wrapper on hero section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import { Separator } from "@/components/ui/separator"
 export default function Home() {
     return (
         <>
-            <section className="w-full h-screen">
-                <div className="container mx-auto h-full bg-custom-base flex flex-col items-center justify-center">
+            <section className="w-full h-screen lg:flex lg:items-center lg:justify-center">
+                <Separator orientation="vertical" className="hidden lg:block"/>
+                <div className="container mx-auto h-full bg-custom-base flex flex-col items-center justify-center lg:justify-start lg:mx-0">
                     <Separator orientation="horizontal"/>
-                    <div className="w-full h-1/2 bg-linear-60 from-custom-base to-custom-base-foregrouund flex flex-col items-center justify-center gap-6 px-6">
+                    <div className="w-full h-1/2 bg-custom-base flex flex-col items-center justify-center gap-6 px-6">
                         <div className="w-fit px-6 py-1 bg-[#DBEAFE] rounded-full">
                             <p className="text-base text-[#155DFC] font-main-semibold">fast, flexible, and ready-to-use digital solutions</p>
                         </div>
@@ -28,7 +29,12 @@ export default function Home() {
                         </div>
                     </div>
                     <Separator orientation="horizontal"/>
+                    <div className="hidden w-full h-1/2 lg:flex bg-custom-base p-6 items-center justify-between gap-6">
+                        <div className="w-1/2 h-full rounded-xl ring-2 ring-custom-accent bg-linear-45 from-custom-base to-custom-secondary"></div>
+                        <div className="w-1/2 h-full rounded-xl ring-2 ring-custom-accent bg-linear-45 from-custom-base to-custom-secondary"></div>
+                    </div>
                 </div>
+                <Separator orientation="vertical" className="hidden lg:block"/>
             </section>
         </>
     )
