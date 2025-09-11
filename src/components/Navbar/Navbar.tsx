690e8a8fd52457d0cc27fc5466182281b2ce8152
farrel irawan <irawanssfarrel@gmail.com>
2025-09-11 14:01:06 +0700

    remaking custom component navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,46 +1,57 @@
+"use client"
+
+import { useState } from "react"
+
 import Link from "next/link"
+
 import { Separator } from "../ui/separator"
+import { MenuIcon, XIcon } from "lucide-react"
 
 export default function Navbar() {
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
         <>
-            <header className="w-full h-16 bg-custom-base">
-                <nav className="container mx-auto h-full flex items-center justify-between bg-custom-base">
-                    <div className="w-fit">
-                        <p className="text-custom-primary font-alternate-bold text-3xl">solid</p>
-                    </div>
-                    <div className="w-4/5">
-                        <ul className="w-full flex items-center justify-center gap-6">
-                            <li>
-                                <Link href={"#about"}>
-                                    <p className="font-main-medium text-base">about us</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href={"#about"}>
-                                    <p className="font-main-medium text-base">services</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href={"#about"}>
-                                    <p className="font-main-medium text-base">solutions</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href={"#about"}>
-                                    <p className="font-main-medium text-base">products</p>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="w-fit">
-                        <div className="w-fit px-6 py-2.5 bg-custom-secondary rounded-full">
-                            <p className="text-custom-primary font-main-semibold">contact us</p>
-                        </div>
-                    </div>
+            <header className="w-full bg-custom-base h-14 px-2.5">
+                <nav className="w-full h-full flex items-center justify-between">
+                    <h2 className="text-3xl font-alternate-bold text-custom-primary">solid</h2>
+                    {
+                        open
+                        ? <XIcon className="w-7 h-7 text-black cursor-pointer" onClick={() => setOpen(false)}/>
+                        : <MenuIcon className="w-6 h-6 text-black cursor-pointer" onClick={() => setOpen(true)}/>
+                    }
                 </nav>
-                <Separator orientation="horizontal"/>
             </header>
+            <Separator orientation="horizontal"/>
+            <div className={`absolute top-0 -translate-y-full w-full h-72 bg-custom-base p-3 flex flex-col items-center justify-between transition-all duration-500 ${open ? "top-14.5 translate-y-0" : ""}`}>
+                <div className="w-full h-full">
+                    <ul className="w-full h-full flex flex-col items-start gap-3">
+                        <li>
+                            <Link href={"#about"}>
+                                <p className="text-base font-main-medium capitalize">about us</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={"/solutions"}>
+                                <p className="text-base font-main-medium capitalize">get solutions</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={"/products"}>
+                                <p className="text-base font-main-medium capitalize">our products</p>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="w-full pb-3">
+                    <Link href={"/contact-us"}>
+                        <button className="w-full bg-custom-accent rounded-full py-2">
+                            <p className="text-lg font-main-semibold text-custom-primary">contact us now</p>
+                        </button>
+                    </Link>
+                </div>
+                <Separator orientation="horizontal"/>
+            </div>
         </>
     )
 }
