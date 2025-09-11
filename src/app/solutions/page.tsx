90f24f41cd71efec78f86a7fe55140228704cd54
farrel irawan <irawanssfarrel@gmail.com>
2025-09-11 16:30:45 +0700

    modifying layout and component on solutions route

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -1,5 +1,17 @@
+import { Separator } from "@/components/ui/separator"
+
 export default function SolutionsPage() {
     return (
-        <></>
+        <>
+            <section className="h-screen w-full flex items-center justify-center">
+                <Separator orientation="vertical"/>
+                    <div className="container h-full mx-0 flex justify-center items-center">
+                        <div className="w-full h-1/2"></div>
+                        <Separator />
+                        <div className="w-full h-1/2"></div>
+                    </div>
+                <Separator orientation="vertical"/>
+        </section>
+        </>
     )
 }
