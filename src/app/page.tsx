b19e3e5a25eb04ddb33b4d736b968c29b33ef4ce
farrel irawan <irawanssfarrel@gmail.com>
2025-09-12 00:55:43 +0700

    memperbarui tampilan UI hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,9 @@ import { GitMergeIcon, MoveRightIcon } from "lucide-react"
 export default function Home() {
     return (
         <>
-            <section className="w-full h-screen lg:flex lg:items-center lg:justify-center">
-                <Separator orientation="vertical" className="hidden lg:block"/>
-                <div className="container mx-auto h-full bg-custom-base flex flex-col items-center justify-center lg:justify-start lg:mx-0">
-                    <Separator orientation="horizontal"/>
-                    <div className="w-full h-1/2 bg-custom-base flex flex-col items-center justify-center gap-6 px-6">
+            <section className="w-full h-screen bg-pattern">
+                <div className="container mx-auto h-full">
+                    <div className="w-full h-full flex flex-col items-center justify-center gap-6 px-6">
                         <div className="w-fit px-6 py-1 bg-[#DBEAFE] rounded-full lg:py-2.5">
                             <p className="text-base text-[#155DFC] font-main-semibold">fast, flexible, and ready-to-use digital solutions</p>
                         </div>
@@ -35,13 +33,7 @@ export default function Home() {
                             </Link>
                         </div>
                     </div>
-                    <Separator orientation="horizontal"/>
-                    <div className="hidden w-full h-1/2 lg:flex bg-custom-base p-6 items-center justify-between gap-6">
-                        <div className="w-1/2 h-full rounded-xl ring-2 ring-custom-accent bg-linear-45 from-custom-base to-custom-secondary"></div>
-                        <div className="w-1/2 h-full rounded-xl ring-2 ring-custom-accent bg-linear-45 from-custom-base to-custom-secondary"></div>
-                    </div>
                 </div>
-                <Separator orientation="vertical" className="hidden lg:block"/>
             </section>
             <section className="w-full h-36 flex flex-col items-center justify-between">
                 <Separator orientation="horizontal"/>
