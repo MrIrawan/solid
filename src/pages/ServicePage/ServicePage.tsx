d88caf47e9d9d8fdfece3b3754cccf8890edf826
farrel irawan <irawanssfarrel@gmail.com>
2025-09-15 10:24:42 +0700

    menambahkan komponen carousel dan card di page core feature

--- src/pages/ServicePage/ServicePage.tsx
+++ src/pages/ServicePage/ServicePage.tsx
@@ -3,6 +3,8 @@ import { staticFeatures } from "../../../public/data/static-features";
 
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle, CardDescription, CardAction } from "@/components/ui/card";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 
 import { ArrowUpRightIcon, ZapIcon } from "lucide-react";
 import { UnplugIcon } from "lucide-react";
@@ -17,7 +19,7 @@ export default function ServicePage() {
                     <h2 className="text-4xl font-alternate-bold max-w-lg text-center">everything your team needs to work smarter</h2>
                     <p className="text-base font-main-medium max-w-xl text-center">our's service have a many impact on your team's and don't worry we here to help if your team don't understand</p>
                 </div>
-                <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:w-2/3 lg:mx-auto">
+                <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:w-2/3 lg:mx-auto lg:hidden">
                     {staticFeatures().map((feature, index) => (
                         <article 
                             className="w-full ring ring-custom-accent h-96 rounded-xl flex flex-col overflow-hidden relative group"
@@ -55,6 +57,27 @@ export default function ServicePage() {
                         </article>
                     ))}
                 </div>
+                <Carousel className="hidden w-full bg-red-300 lg:block">
+                    <CarouselContent className="w-1/2">
+                        {staticFeatures().map((feature, index) => (
+                            <CarouselItem key={index}>
+                                <Card className="w-full h-full">
+                                    <CardHeader className="w-full h-1/2 bg-red-500 overflow-hidden">
+                                        <Image 
+                                            src={feature.image}
+                                            alt={`image-${index}`}
+                                            width={500}
+                                            height={500}
+                                            className="w-full h-full object-cover"
+                                        />
+                                    </CardHeader>
+                                </Card>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
             </div>
         </section>
     )
