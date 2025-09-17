6a7965d5d3cfd2972d2bf9d5d2164aa088dff648
farrel irawan <irawanssfarrel@gmail.com>
2025-09-17 15:52:38 +0700

    membuat custom component MessageBox dan membuat props interface MessageBox

---
+++ src/components/MessageBox/MessageBox.tsx
@@ -0,0 +1,11 @@
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
+
+export default function MessageBox() {
+    return (
+        <>
+            <Label></Label>
+            <Textarea />
+        </>
+    )
+}
--- src/types/customComponentsTypes.ts
+++ src/types/customComponentsTypes.ts
@@ -1,8 +1,12 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 
 export interface InputGroupProps extends InputHTMLAttributes<HTMLInputElement> {
     labelStyle: string;
     htmlFor: string;
     children: React.ReactNode;
     inputStyle: string;
+}
+
+export interface MessageBoxProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+    children: React.ReactNode;
 }
