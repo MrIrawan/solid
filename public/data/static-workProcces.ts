913a48026512a877e22393738a8561f1d25d8331
farrel irawan <irawanssfarrel@gmail.com>
2025-09-25 20:30:16 +0700

    membuat file static-workProccess untuk menampung data static halaman work proccess

---
+++ public/data/static-workProcces.ts
@@ -0,0 +1,40 @@
+import { staticWorkProccessData } from "@/types/types";
+
+import Image1 from "../../public/images/Consult Experts.svg"
+import Image2 from "../../public/images/Conversation Businessman Customer 1.svg"
+import Image3 from "../../public/images/Problem Solving 3.svg"
+import Image4 from "../../public/images/Marketer Giving A Key Note 5.svg"
+import Image5 from "../../public/images/Evaluate Performance Employee 3.svg"
+
+export const staticData: staticWorkProccessData[] = [
+    {
+        id: 1,
+        title: "Discovery & Consultation",
+        description: "We begin by understanding your business goals, challenges, and opportunities in the digital space.",
+        image: Image1.src
+    },
+    {
+        id: 2,
+        title: "Strategy Design",
+        description: "We develop a clear, tailored strategy and roadmap for your digital solution.",
+        image: Image1.src
+    },
+    {
+        id: 3,
+        title: "Implementation",
+        description: "Our team executes the plan — deploying technology, building assets, and integrating systems seamlessly.",
+        image: Image1.src
+    },
+    {
+        id: 4,
+        title: "Optimization",
+        description: "We review performance, fine-tune your digital tools, and ensure everything delivers maximum value.",
+        image: Image1.src
+    },
+    {
+        id: 5,
+        title: "Support & Growth",
+        description: "We provide ongoing support and fresh ideas to keep your business ahead in the digital era.",
+        image: Image1.src
+    },
+]
