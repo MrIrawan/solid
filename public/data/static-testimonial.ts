0920df93944bf46ba73803f82944b49823cefc8a
farrel irawan <irawanssfarrel@gmail.com>
2025-09-25 11:35:34 +0700

    modifikasi halaman testimonial dan menambahkan aset gambar untuk halaman testimonial

--- public/data/static-testimonial.ts
+++ public/data/static-testimonial.ts
@@ -1,39 +1,53 @@
 import { staticTestimonialData } from "@/types/types";
 
+import Avatar1 from "../images/sarah-williams.jpg"
+import Avatar2 from "../images/michael-chen.jpg"
+import Avatar3 from "../images/alicia-gomez.jpg"
+import Avatar4 from "../images/david-lee.jpg"
+import Avatar5 from "../images/emma-brown.jpg"
+import Avatar6 from "../images/eddie-zhang.jpg"
+
 export const testimonialData: staticTestimonialData[] = [
     {
         name: "Sarah Williams",
         role: "CEO, TechNova",
-        avatar: "/images/avatar1.png",
+        avatar: Avatar1.src,
         testimonial: "Solid transformed our digital workflow. Their team is fast, reliable, and always delivers beyond expectations. Highly recommended!",
         stars: 5
     },
     {
         name: "Michael Chen",
         role: "Product Manager, FinEdge",
-        avatar: "/images/avatar2.png",
+        avatar: Avatar2.src,
         testimonial: "We trusted Solid with our core business platform. The results were outstanding—secure, flexible, and beautifully designed.",
         stars: 4
     },
     {
         name: "Alicia Gomez",
         role: "Marketing Lead, Creatify",
-        avatar: "/images/avatar3.png",
+        avatar: Avatar3.src,
         testimonial: "Working with Solid was a game changer. Their solutions helped us scale faster and reach new markets with confidence.",
         stars: 5
     },
     {
         name: "David Lee",
         role: "CTO, HealthSync",
-        avatar: "/images/avatar4.png",
+        avatar: Avatar4.src,
         testimonial: "Solid’s expertise in digital solutions is unmatched. The support and attention to detail made our launch seamless.",
         stars: 3
     },
     {
         name: "Emma Brown",
         role: "Founder, EduSpark",
-        avatar: "/images/avatar5.png",
+        avatar: Avatar5.src,
         testimonial: "From consultation to delivery, Solid exceeded our expectations. Our users love the new platform!",
         stars: 4
-    }
+    },
+    {
+        name: "eddie zhang",
+        role: "marketing lead, Be The One",
+        avatar: Avatar6.src,
+        testimonial: "lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nemo nulla dignissimos, qui enim ad?",
+        stars: 4
+    },
 ]
